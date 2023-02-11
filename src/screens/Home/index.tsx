c4e3c5c819d8f5428b1370988e0251aa03192971
Marcelo <marcelo3x4@hotmail.com>
2023-02-11 15:55:25 -0300

    pushing for gitHub

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,33 @@
+import React, {useState} from "react";
+
 import { View, Text, TextInput, TouchableOpacity, FlatList, Alert} from "react-native";
 
 import { Participant } from "../../components/Participant";
 import { styles } from "./styles"
 
 export function Home() {
-  const participants = ['João']
-  
+  const [participants, setParticipants] = useState<string[]>([])
+  const [participantsName, setParticipantsName] = useState('')
+
   function handleParticipantAdd() {
-
-    if(participants.includes("Marcelo")){
-      return Alert.alert("participante existente", "Já existe um participante na lista com esse nome")
-
+    if(participants.includes(participantsName)){
+      return Alert.alert("participante Existente",`${participantsName} já está na lista`)
     }
-    participants.push("ana")
+    if(participantsName === ''){
+      return Alert.alert("Erro", "escreva um nome para adicionar")
+    }
 
-    console.log(participants)
+    setParticipants(prevState => [...prevState, participantsName])
+    setParticipantsName('')
+
   }
 
   function handleParticipantRemove(name: String) {
+
     Alert.alert("Remover", `Deseja remover o participante ${name}`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado!")
+        onPress: () => setParticipants(currentState => currentState.filter(participant => participant !== name))
       },
       {
         text: 'Não',
@@ -29,16 +35,27 @@ export function Home() {
       }
     ])
   }
+  function showDateCurrent(){
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0') // 1 -> 01
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+
+    const year = date.getFullYear()
+
+    const dateCurrent = `${day}/${month}/${year}`
+
+    return dateCurrent
+  }
 
   return (
     <View style={styles.container}> 
 
       <Text style={styles.eventName}>
-        Nome do evento
+      Mega Circle Pit
       </Text>
 
       <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022
+        {showDateCurrent()}
       </Text>
 
       <View style={styles.form}>
@@ -46,7 +63,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#FFFF"
-      
+          onChangeText={setParticipantsName}
+          value={participantsName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>
